c03bebb7be6649d043c89c82496c5b50a02c448b
trabalho01-python <trabalho74549python@gamil.com>
2025-09-20 19:05:16 -0300

    Arquivos iniciais para deploy PA

---
+++ README.md
@@ -0,0 +1,5 @@
+python -m venv .venv
+
+.venv\Scripts\Activate
+
+pip isntall -r requirements.txt
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-import mysql.connector as my
+
 
 app = Flask(__name__)
 
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-
 @app.route('/pagina2')
 def pagina2():
     return render_template('pagina2.html')
+
 app.run(debug=True)
