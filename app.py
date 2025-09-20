44730fccd61d8b90fb22d020f2478ba16db6468d
trabalho01-python <trabalho74549python@gamil.com>
2025-09-20 17:54:56 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector as my
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.run(debug=True)
